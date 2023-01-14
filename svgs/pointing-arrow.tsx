67d978dacbc5cac5894e02a18afada84d99e488a
jibola <pro.ajibolaojo@gmail.com>
2023-01-14 23:45:16 +0100

    remoced svg dashes

--- svgs/pointing-arrow.tsx
+++ svgs/pointing-arrow.tsx
@@ -13,9 +13,9 @@ export function PointingArrow(props: SVGProps<SVGSVGElement>) {
       <path
         d="M17.3785 32.3097C76.5333 14.2418 107 57.5 72 69C48.0366 76.8737 38.539 19.1654 91.7929 27.5971C134.396 34.3424 112.42 88.0187 96.1066 114.014"
         stroke="#1F1F1F"
-        stroke-width="3"
+        strokeWidth="3"
       />
-      <path d="M93 101L95.5 114.5L109 112" stroke="#1F1F1F" stroke-width="3" />
+      <path d="M93 101L95.5 114.5L109 112" stroke="#1F1F1F" strokeWidth="3" />
     </svg>
   );
 }
--- svgs/scroll-up.tsx
+++ svgs/scroll-up.tsx
@@ -13,7 +13,7 @@ export function ScrollUp(props: SVGProps<SVGSVGElement>) {
       <path
         d="M45.7805 60.1698L45.7805 25.5M45.7805 25.5C47.2943 28.3616 52.6576 34.4811 62 36.066M45.7805 25.5L45.7805 66M45.7805 25.5C44.2667 28.3616 38.9034 34.4811 29.561 36.066"
         stroke="#1F1F1F"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <circle
         cx="45.5"
@@ -21,7 +21,7 @@ export function ScrollUp(props: SVGProps<SVGSVGElement>) {
         r="44.75"
         transform="rotate(-180 45.5 45.5)"
         stroke="#1F1F1F"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
     </svg>
   );
